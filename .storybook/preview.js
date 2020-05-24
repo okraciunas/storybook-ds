0d25c2ef6f88cc42ab709a6ec1cbd46238267b75
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-23 22:26:30 -0300

    Adiciona extra A11Y

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
+import { withA11Y } from "@storybook/addon-a11y";
+
 import { GlobalStyle } from "./../src/shared/global";
+
+addDecorator(withA11Y);
 
 addDecorator((story) => (
   <>
